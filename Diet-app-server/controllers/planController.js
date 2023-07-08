d05841860d2159f03493956405c2146a20d8084f
preethichals <108888412+preethichals@users.noreply.github.com>
2023-07-08 21:34:36 +0530

    Add files via upload

---
+++ Diet-app-server/controllers/planController.js
@@ -0,0 +1,264 @@
+import fs from "fs";
+import slugify from "slugify";
+import planModel from "../models/planModel.js";
+import categoryModel from "../models/categoryModel.js";
+
+// Create Controller
+export const createPlanController = async (req, res) => {
+  try {
+    const { name, category, description } = req.fields;
+    const { sampleimage } = req.files;
+    //validation
+    switch (true) {
+      case !name:
+        return res.status(500).send({ error: "Name is Required" });
+      case !category:
+        return res.status(500).send({ error: "Category is Required" });
+      case !description:
+        return res.status(500).send({ error: "Description is Required" });
+      case sampleimage && sampleimage.size > 1000000:
+        return res.status(500).send({
+          error: "Sample Image is Required and size should be less than 1mb",
+        });
+    }
+    const plan = new planModel({
+      ...req.fields,
+      slug: slugify(name),
+    });
+    if (sampleimage) {
+      plan.sampleimage.data = fs.readFileSync(sampleimage.path);
+      plan.sampleimage.contentType = sampleimage.type;
+    }
+
+    await plan.save();
+    res.status(201).send({
+      success: true,
+      message: "New Plan created!",
+      plan,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      error,
+      message: "Error in creating Plan",
+    });
+  }
+};
+
+//Get all Plans
+export const getPlanController = async (req, res) => {
+  try {
+    const plan = await planModel
+      .find({})
+      .populate("category")
+      .select("-sampleimage")
+      .limit(12)
+      .sort({ createdAt: -1 });
+    res.status(200).send({
+      success: true,
+      countTotal: plan.length,
+      message: "All Plans",
+      plan,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      error,
+      message: "Error in getting plans",
+    });
+  }
+};
+
+//Get Single plan
+
+export const getSinglePlanController = async (req, res) => {
+  try {
+    const plan = await planModel
+      .findOne({ slug: req.params.slug })
+      .select("-sampleimage")
+      .populate("category");
+    res.status(200).send({
+      success: true,
+      message: "Single plan fetched",
+      plan,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      error,
+      message: "Error while getting single plan",
+    });
+  }
+};
+
+//Get Sample-image
+
+export const planSampleImageController = async (req, res) => {
+  try {
+    const plan = await planModel.findById(req.params.pid).select("sampleimage");
+    if (plan.sampleimage.data) {
+      res.set("Content-type", plan.sampleimage.contentType);
+      return res.status(200).send(plan.sampleimage.data);
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: " Error whitle getting image",
+      error,
+    });
+  }
+};
+
+//Delete controller
+export const deletePlanController = async (req, res) => {
+  try {
+    await planModel.findByIdAndDelete(req.params.pid).select("-sampleimage");
+    res.status(200).send({
+      success: true,
+      message: "Plan deleted succfully",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Error while deleting plan",
+      error,
+    });
+  }
+};
+
+//Update Controller
+
+export const updatePlanController = async (req, res) => {
+  try {
+    const { name, category, description } = req.fields;
+    const { sampleimage } = req.files;
+    //validation
+    switch (true) {
+      case !name:
+        return res.status(500).send({ error: "Name is Required" });
+      case !category:
+        return res.status(500).send({ error: "Category is Required" });
+      case !description:
+        return res.status(500).send({ error: "Description is Required" });
+      case sampleimage && sampleimage.size > 1000000:
+        return res.status(500).send({
+          error: "Sample Image is Required and size should be less than 1mb",
+        });
+    }
+    const plan = await planModel.findByIdAndUpdate(
+      req.params.pid,
+      { ...req.fields, slug: slugify(name) },
+      { new: true }
+    );
+    if (sampleimage) {
+      plan.sampleimage.data = fs.readFileSync(sampleimage.path);
+      plan.sampleimage.contentType = sampleimage.type;
+    }
+
+    await plan.save();
+    res.status(201).send({
+      success: true,
+      message: "Plan updated successfully",
+      plan,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      error,
+      message: "Error in updating Plan",
+    });
+  }
+};
+
+//Plan-Filter Controller
+
+export const planFilterController = async (req, res) => {
+  try {
+    const { checked } = req.body;
+    let args = {};
+    if (checked.length > 0) args.category = checked;
+    const plan = await planModel.find(args);
+    res.status(200).send({
+      success: true,
+      plan,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).send({
+      success: false,
+      message: "Error While filtering meal plans",
+      error,
+    });
+  }
+};
+
+//Plan count Controller
+
+export const planCountController = async (req, res) => {
+  try {
+    const total = await planModel.find({}).estimatedDocumentCount();
+    res.status(200).send({
+      success: true,
+      total,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).send({
+      success: false,
+      message: "Error in getting count",
+      error,
+    });
+  }
+};
+
+// Plan list per Page COntroller
+
+export const planPageListController = async (req, res) => {
+  try {
+    const perPage = 6;
+    const page = req.params.page ? req.params.page : 1;
+    const plan = await planModel
+      .find({})
+      .select("-sampleimage")
+      .skip((page - 1) * perPage)
+      .limit(perPage)
+      .sort({ createdAt: -1 });
+    res.status(200).send({
+      success: true,
+      plan,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).send({
+      success: false,
+      message: "Error in getting page",
+      error,
+    });
+  }
+};
+
+//Category wise plan
+export const planCategoryController = async (req, res) => {
+  try {
+    const category = await categoryModel.findOne({ slug: req.params.slug });
+    const plans = await planModel.find({ category }).populate("category");
+    res.status(200).send({
+      success: true,
+      category,
+      plans,
+    });
+  } catch (error) {
+    console.log(error);
+    res.success(400).send({
+      success: false,
+      error,
+      message: "Error While getting plans",
+    });
+  }
+};
---
+++ Diet-app-server/models/planModel.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose'
+
+const planSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+    },
+    slug:{
+        type:String,
+        required:true,
+    },
+    category:{
+        type:mongoose.ObjectId,
+        ref:'Category',
+        required:true
+    },
+    description:{
+        type:String,
+        required:true,
+       
+    },
+    sampleimage:{
+        data:Buffer,
+        contentType:String
+    }
+},
+{timestamps:true})
+export default mongoose.model("Plans", planSchema)
